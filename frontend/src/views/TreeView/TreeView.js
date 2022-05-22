2eb72f9f846e0ef17c8e7abde4400f80ffa94dec
Kuba <kubaskaw000@gmail.com>
2022-05-22 15:14:41 +0200

    Naprawione poruszanie po drzewie

--- frontend/src/views/TreeView/TreeView.js
+++ frontend/src/views/TreeView/TreeView.js
@@ -9,12 +9,14 @@ import "./TreeView.css";
 const TreeView = () => {
   const [data, setData] = useState([]);
   const [path, setPath] = useState([]);
+  const [branches, setBranches] = useState([]);
   const [moveId, setMoveId] = useState(1);
   const [boardFen, setBoardFen] = useState(
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
   );
 
   useEffect(() => {
+    console.log(branches);
     const getGamesByPath = async (path, moveId) => {
       try {
         const response = await fetch(
@@ -27,6 +29,14 @@ const TreeView = () => {
 
         //console.log(resJson.data);
         setData(resJson.data);
+        //console.log(branches);
+        //console.log(data);
+
+        let branch = resJson.data;
+
+        setBranches([...branches, branch]);
+
+        console.log(branches);
       } catch (err) {
         console.log(err);
       }
@@ -35,29 +45,50 @@ const TreeView = () => {
     getGamesByPath(path, moveId);
   }, [path]);
 
-  //console.log(data);
+  //console.log(branches[0]);
 
   return (
     <>
-      <div className="moves">
-        {data.map((move, i) => (
-          <div
-            key={i}
-            className="move"
-            onClick={() => {
-              let newPath = [...path, move.move];
-              setPath(newPath);
-              setMoveId(moveId + 1);
-              setBoardFen(move.fen);
-              console.log(path, moveId);
-            }}
-          >
-            <div className="move-value">{move.move}</div>
-            <div className="move-count">{move.count}</div>
+      <div className="branches">
+        {branches.map((branch, branchId) => (
+          <div key={branchId} className="branch">
+            <div className="moves">
+              {branch.map((move, i) => (
+                <div
+                  key={i}
+                  className="move"
+                  onClick={() => {
+                    let newPath = [...path, move.move];
+
+                    setMoveId(branchId + 2);
+                    setPath(newPath);
+                    setBoardFen(move.fen);
+
+                    if (moveId - branchId != 1) {
+                      let newPath2 = newPath.splice(0, branchId);
+
+                      setPath([...newPath2, move.move]);
+                      setMoveId(newPath2.length + 2);
+                      let newBranches = [...branches].slice(0, branchId + 1);
+                      setBranches(newBranches);
+
+                      console.log(moveId, branchId);
+                      console.log(newPath2);
+                    }
+
+                    //console.log(path);
+                    //console.log(path, moveId);
+                  }}
+                >
+                  <div className="move-value">{move.move}</div>
+                  <div className="move-count">{move.count}</div>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
-        <Board fen={boardFen} />;
       </div>
+      <Board fen={boardFen} />;
     </>
   );
 };
